49670f1abee9cf9a3b5fafd689634f4e06117053
Ankush Sharma <ankush.sharma.live@gmail.com>
2022-05-26 13:07:27 +0530

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,23 @@
+<div class="footer">
+    <div class="page-container footer-container">
+        <div class="footer-cities">
+            <div class="footer-city">
+                <a href="property_list.php?city=Sanjauli">PG in Sanjauli</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Summer Hill">PG in Summer Hill</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Chotta Shimla">PG in Chotta Shimla</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Lakkar Bazar">PG in Lakkar Bazar</a>
+            </div>
+        </div>
+        <div class="footer-copyright">© 2022 Copyright PG Life </div>
+    </div>
+</div>
+
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/common.js"></script>
